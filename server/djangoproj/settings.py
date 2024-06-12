fed340b9df232d16e09f076509406ddea923fa56
Martin <Martin@testmail.com>
2024-06-12 16:26:04 -0400

    Add CarMake and CarModel models and implement get_cars endpoint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://martinruebne-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://martinruebne-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS=['*']
+CSRF_TRUSTED_ORIGINS = [
+    'https://martinruebne-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://martinruebne-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', 
+    'https://martinruebne-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/admin'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
