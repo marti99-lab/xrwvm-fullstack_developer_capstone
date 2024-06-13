a587c852ac541e909abdcac9c99198ab7cc85f85
marti99-lab <114484259+marti99-lab@users.noreply.github.com>
2024-06-13 18:39:32 +0200

    Delete djangoapp directory
    delete wrong files 2

--- djangoapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CarMake, CarModel
-
-# Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
